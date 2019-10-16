6e104d9f1ad44881844edf47462d7b1783b7fc99
Ahmad Abdulal <37836141+ahmadHKR@users.noreply.github.com>
2019-10-16 15:27:53 +0200

    initial commit
    adding the main classes for both boards Arduino and ESP8266

---
+++ Arduino-Genuino-Uno/Arduino-Genuino-Uno.ino
@@ -0,0 +1,18 @@
+/*
+ * This is the main class to handle the arduino board functionalities
+ * and to implement them in a specific order. These functionalities are as such as:
+ * - Retriving data from the esp8266 board
+ * - translate this data and converte it into states
+ * - Apply these states into the connected devices with arduino
+ */
+
+
+//TODO 
+void setup() {
+
+}
+
+//TODO get data from esp, handle errors, apply the new states 
+void loop() {
+  
+}
---
+++ Lolin-ESP8266-01-V3/Lolin-ESP8266-01-V3.ino
@@ -0,0 +1,17 @@
+/*
+ * This is the main class to handle the esp8266 board functionalities
+ * and to implement them in a specific order. These functionalities are such as:
+ *  - Establishing wifi connection
+ *  - Establishing connection with server
+ *  - Handle communication errors
+ *  - Receive data from server and forward it to the arduino board
+ */
+
+//TODO connect to wifi,establish connection with the server
+void setup() {
+
+}
+//TODO get data from server, check if data is valid and usable, send data to arduino
+void loop() {
+  
+}
