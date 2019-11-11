5cc2fbd940358f2588c045f33f26f0d3f6a3e58c
Ahmad Abdulal <37836141+ahmadHKR@users.noreply.github.com>
2019-11-11 12:19:15 +0100

    updating ESP
    Fixed an issue where ESP didnt sync correctly with arduino

--- Lolin-ESP8266-01-V3/Lolin-ESP8266-01-V3.ino
+++ Lolin-ESP8266-01-V3/Lolin-ESP8266-01-V3.ino
@@ -7,7 +7,7 @@
     - Receive data from server and forward it to the arduino board
 */
 
-#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+#include <ESP8266WiFi.h>
 #include "RestClient.h"
 
 const char* ssid     = "ASUS_2G";
@@ -16,43 +16,41 @@ const char* password = "greatdaisy150";
 //TODO connect to wifi,establish connection with the server
 void setup() {
 
-  Serial.begin(115200); // Start the Serial communication to send messages to the computer
-  Serial.println("AT+IPR=9600");
+  Serial.begin(115200); // start serial with the ESP defualt baud rate
+  Serial.println("AT+IPR=9600"); // change the defualt baud rate to 9600
   delay(1000);
   Serial.end();
-  Serial.begin(9600);
+  Serial.begin(9600); // serial is ready to communicate with arduino
   delay(10);
-  
-  Serial.println('\n');
+  Serial.setTimeout(266);
 
-  WiFi.begin(ssid, password);             // Connect to the network
+  //TODO put this loop in a method so we call it whenever the connection is lost
+  WiFi.begin(ssid, password);  // Connect to the network
+  Serial.print('\n');
   Serial.print("Connecting to ");
   Serial.print(ssid); Serial.println(" ...");
 
   int i = 0;
+
   while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
     delay(1000);
     Serial.print(++i); Serial.print(' ');
   }
 
-  Serial.println('\n');
+  Serial.print('\n');
   Serial.println("Connection established!");
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());
+  Serial.flush();
   // end wifi connection
 }
 
 //TODO get data from server, check if data is valid and usable, send data to arduino
 void loop() {
-  
-  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
-   
-          RestClient rest;
-          Serial.println(rest.getDevices());
-          Serial.flush();  
-    }
-    
-    delay(2000);    //Send a request every 30 seconds
-    }
 
-  
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+      RestClient rest;
+      Serial.println(rest.getDevices());
+      Serial.flush();
+  }
+}
