5df247a8fffc4bf0537fcf25ebb37fdfef87749a
Ahmad Abdulal <37836141+AHAB-HUB@users.noreply.github.com>
2019-11-29 21:15:48 +0100

    Updating ESP
    The performance has been improved to read data correctly from serial. Fixed an issue where the wifi wont reconnect to the network upon disconnection.

--- Lolin-ESP8266-01-V3/Lolin-ESP8266-01-V3.ino
+++ Lolin-ESP8266-01-V3/Lolin-ESP8266-01-V3.ino
@@ -10,47 +10,66 @@
 #include <ESP8266WiFi.h>
 #include "RestClient.h"
 
-const char* ssid     = "NETGEAR68";
-const char* password = "greatdaisy160";
+const char* ssid     = "ASUS_2G";
+const char* password = "greatdaisy150";
+RestClient rest;
+bool newData = false;
+String recData;
 
-//TODO connect to wifi,establish connection with the server
 void setup() {
 
   Serial.begin(115200); // start serial with the ESP defualt baud rate
-  Serial.println("AT+IPR=9600"); // change the defualt baud rate to 9600
   delay(1000);
-  Serial.end();
-  Serial.begin(9600); // serial is ready to communicate with arduino
-  delay(10);
+  Serial.flush();
 
-  //TODO put this loop in a method so we call it whenever the connection is lost
-  WiFi.begin(ssid, password);  // Connect to the network
+  establishWifiConnection("Connecting to ");
+}
+
+void loop() {
+
+  if ( WiFi.status() == WL_CONNECTED ) {
+    receiveData();
+
+    if (recData == "ready") {
+      Serial.write(rest.getDevices().c_str());
+      Serial.write('\n');
+    } else {
+      serializeAndPost();
+    }
+  } else {
+    establishWifiConnection("Wifi connection lost! Attempting to reconnect.. ");
+  }
+}
+
+void receiveData() {
+  while (Serial.available() == 0) {}
+
+  recData =  Serial.readStringUntil('\n');
+}
+
+void serializeAndPost() {
+  Serial.write("recevied from serial: ");
+  Serial.write(recData.c_str());
+  Serial.write('\n');
+}
+
+void establishWifiConnection(String message) {
+
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.disconnect();
+    WiFi.begin(ssid, password);
+  }
   Serial.print('\n');
-  Serial.print("Connecting to ");
+  Serial.print(message);
   Serial.print(ssid); Serial.println(" ...");
 
-  int i = 0;
-
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+  for ( int i = 1; WiFi.status() != WL_CONNECTED; i++ ) {
     delay(1000);
-    Serial.print(++i); Serial.print(' ');
+    Serial.print(i); Serial.print(' ');
   }
 
   Serial.print('\n');
   Serial.println("Connection established!");
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());
-  Serial.flush();
-  // end wifi connection
-}
-
-//TODO get data from server, check if data is valid and usable, send data to arduino
-void loop() {
-
-  if (WiFi.status() == WL_CONNECTED && Serial.availableForWrite() ) { //Check WiFi connection status
-      RestClient rest;
-      Serial.println(rest.getDevices());
-  }
-
-  delay(1000); // request data each 1 sec
 }
